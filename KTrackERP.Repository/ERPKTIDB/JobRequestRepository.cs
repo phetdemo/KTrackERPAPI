2ec91126ab1f0b8c035b41f5d3939e55e0bde569
RiderS1000RR <joker-acc@hotmail.com>
2020-01-25 12:48:49 +0700

    .

--- KTrackERP.Repository/ERPKTIDB/JobRequestRepository.cs
+++ KTrackERP.Repository/ERPKTIDB/JobRequestRepository.cs
@@ -164,124 +164,132 @@ namespace KTrackERP.Repository.ERPKTIDB
 
         public bool Insert(JobRequest model)
         {
-            using (var dbTransaction = context.Database.BeginTransaction())
+            //using (var dbTransaction = context.Database.BeginTransaction())
+            //{
+            try
             {
-                try
+                if (model != null)
                 {
-                    if (model != null)
+
+                    var update = context.JobRequest.Where(x => x.JobRequestNoID == model.JobRequestNoID).FirstOrDefault();
+
+                    if (update != null)
                     {
+                        update.AccountFlag = model.AccountFlag;
+                        update.AppointmentDateTime = model.AppointmentDateTime;
+                        update.AppointmentDetail = model.AppointmentDetail;
+                        update.CompanyName = model.CompanyName;
+                        update.ContactFax = model.ContactFax;
+                        update.ContactMobile = model.ContactMobile;
+                        update.ContactName = model.ContactName;
+                        update.ContactTel = model.ContactTel;
+                        update.CostFlag = model.CostFlag;
+                        update.CostOtherDetail = model.CostOtherDetail;
+                        update.CostOtherFlag = model.CostOtherFlag;
+                        update.CustomerSupportFlag = model.CustomerSupportFlag;
+                        update.EndContactDate = model.EndContactDate;
+                        update.HardWareFlag = model.HardWareFlag;
+                        update.JobRef = model.JobRef;
+                        update.JobRequestNo = model.JobRequestNo;
+                        update.JobRequestType = model.JobRequestType;
+                        update.JobStatus = model.JobStatus;
+                        update.Location = model.Location;
+                        update.OtherDetail = model.OtherDetail;
+                        update.ProcessDateTime = model.ProcessDateTime;
+                        update.Remark = model.Remark;
+                        update.RentTypeID = model.RentTypeID;
+                        update.SaleCoordinatorID = model.SaleCoordinatorID;
+                        update.Informer = model.Informer;
+                        update.SaleID = model.SaleID;
+                        update.SoftWareFlag = model.SoftWareFlag;
+                        update.StartContactDate = model.StartContactDate;
+                        update.UpdBy = model.UpdBy;
+                        update.UpdDateTime = DateTime.Now;
 
-                        var update = context.JobRequest.Where(x => x.JobRequestNoID == model.JobRequestNoID).FirstOrDefault();
-
-                        if (update != null)
+                        foreach (Car item in model.Car)
                         {
-                            update.AccountFlag = model.AccountFlag;
-                            update.AppointmentDateTime = model.AppointmentDateTime;
-                            update.AppointmentDetail = model.AppointmentDetail;
-                            update.CompanyName = model.CompanyName;
-                            update.ContactFax = model.ContactFax;
-                            update.ContactMobile = model.ContactMobile;
-                            update.ContactName = model.ContactName;
-                            update.ContactTel = model.ContactTel;
-                            update.CostFlag = model.CostFlag;
-                            update.CostOtherDetail = model.CostOtherDetail;
-                            update.CostOtherFlag = model.CostOtherFlag;
-                            update.CustomerSupportFlag = model.CustomerSupportFlag;
-                            update.EndContactDate = model.EndContactDate;
-                            update.HardWareFlag = model.HardWareFlag;
-                            update.JobRef = model.JobRef;
-                            update.JobRequestNo = model.JobRequestNo;
-                            update.JobRequestType = model.JobRequestType;
-                            update.JobStatus = model.JobStatus;
-                            update.Location = model.Location;
-                            update.OtherDetail = model.OtherDetail;
-                            update.ProcessDateTime = model.ProcessDateTime;
-                            update.Remark = model.Remark;
-                            update.RentTypeID = model.RentTypeID;
-                            update.SaleCoordinatorID = model.SaleCoordinatorID;
-                            update.Informer = model.Informer;
-                            update.SaleID = model.SaleID;
-                            update.SoftWareFlag = model.SoftWareFlag;
-                            update.StartContactDate = model.StartContactDate;
-                            update.UpdBy = model.UpdBy;
-                            update.UpdDateTime = DateTime.Now;
-
-                            foreach (Car item in model.Car)
+                            var updatecar = context.Car.Where(x => x.CarID == item.CarID).FirstOrDefault();
+                            if (update != null)
                             {
-                                var updatecar = context.Car.Where(x => x.CarID == item.CarID).FirstOrDefault();
-                                if (update != null)
-                                {
-                                    updatecar.CarID = item.CarID;
-                                    updatecar.CarTypeID = item.CarTypeID;
-                                    updatecar.Chassis = item.Chassis;
-                                    updatecar.JobRequestNoID = item.JobRequestNoID;
-                                    updatecar.LicenceDriveTypeID = item.LicenceDriveTypeID;
-                                    updatecar.LicensePlate = item.LicensePlate;
-                                    updatecar.Remark = item.Remark;
-                                    updatecar.Shaft = item.Shaft;
-                                    updatecar.Tire = item.Tire;
-                                    updatecar.UpdBy = item.UpdBy;
-                                    updatecar.UpdDateTime = DateTime.Now;
-                                    updatecar.Wheel = item.Wheel;
-                                }
-                            }
-                            foreach (Box item in model.Box)
-                            {
-                                var updatebox = context.Box.Where(x => x.BoxID == item.BoxID).FirstOrDefault();
-                                if (updatebox != null)
-                                {
-                                    updatebox.AmountCameraDVR = item.AmountCameraDVR;
-                                    updatebox.APN = item.APN;
-                                    updatebox.BatteryID = item.BatteryID;
-                                    updatebox.CarID = item.CarID;
-                                    updatebox.DeviceID = item.DeviceID;
-                                    updatebox.ElectricVoltID = item.ElectricVoltID;
-                                    updatebox.FirmWareID = item.FirmWareID;
-                                    updatebox.IP = item.IP;
-                                    updatebox.LimitSpeedID = item.LimitSpeedID;
-                                    updatebox.Password = item.Password;
-                                    updatebox.Port = item.Port;
-                                    updatebox.SerialDVRNumber = item.SerialDVRNumber;
-                                    updatebox.SerialNumber = item.SerialNumber;
-                                    updatebox.SimTypeID = item.SimTypeID;
-                                    updatebox.SoundAlertID = item.SoundAlertID;
-                                    updatebox.TimeSendDataID = item.TimeSendDataID;
-                                    updatebox.UpdBy = item.UpdBy;
-                                    updatebox.UpdDateTime = DateTime.Now;
-                                    updatebox.Username = item.Username;
-                                    updatebox.VID = item.VID;
-                                    updatebox.warrantydateEnd = item.warrantydateEnd;
-                                    updatebox.warrantydateStart = item.warrantydateStart;
-                                }
+                                updatecar.CarID = item.CarID;
+                                updatecar.CarTypeID = item.CarTypeID;
+                                updatecar.Chassis = item.Chassis;
+                                updatecar.JobRequestNoID = item.JobRequestNoID;
+                                updatecar.LicenceDriveTypeID = item.LicenceDriveTypeID;
+                                updatecar.LicensePlate = item.LicensePlate;
+                                updatecar.Remark = item.Remark;
+                                updatecar.Shaft = item.Shaft;
+                                updatecar.Tire = item.Tire;
+                                updatecar.UpdBy = item.UpdBy;
+                                updatecar.UpdDateTime = DateTime.Now;
+                                updatecar.Wheel = item.Wheel;
                             }
                         }
-                        else
+                        foreach (Box item in model.Box)
                         {
-                            model.InsDateTime = DateTime.Now;
-                            context.JobRequest.Add(model);
-
-                            for (int i = 0; i < model.Car.Count; i++)
+                            var updatebox = context.Box.Where(x => x.BoxID == item.BoxID).FirstOrDefault();
+                            if (updatebox != null)
                             {
-                                model.Car[i].InsDateTime = DateTime.Now;
-                                context.Car.Add(model.Car[i]);
-                            }
-                            for (int i = 0; i < model.Box.Count; i++)
-                            {
-                                model.Box[i].InsDateTime = DateTime.Now;
-                                context.Box.Add(model.Box[i]);
+                                updatebox.AmountCameraDVR = item.AmountCameraDVR;
+                                updatebox.APN = item.APN;
+                                updatebox.BatteryID = item.BatteryID;
+                                updatebox.CarID = item.CarID;
+                                updatebox.DeviceID = item.DeviceID;
+                                updatebox.ElectricVoltID = item.ElectricVoltID;
+                                updatebox.FirmWareID = item.FirmWareID;
+                                updatebox.IP = item.IP;
+                                updatebox.LimitSpeedID = item.LimitSpeedID;
+                                updatebox.Password = item.Password;
+                                updatebox.Port = item.Port;
+                                updatebox.SerialDVRNumber = item.SerialDVRNumber;
+                                updatebox.SerialNumber = item.SerialNumber;
+                                updatebox.SimTypeID = item.SimTypeID;
+                                updatebox.SoundAlertID = item.SoundAlertID;
+                                updatebox.TimeSendDataID = item.TimeSendDataID;
+                                updatebox.UpdBy = item.UpdBy;
+                                updatebox.UpdDateTime = DateTime.Now;
+                                updatebox.Username = item.Username;
+                                updatebox.VID = item.VID;
+                                updatebox.warrantydateEnd = item.warrantydateEnd;
+                                updatebox.warrantydateStart = item.warrantydateStart;
                             }
                         }
-                        context.SaveChanges();
-                        dbTransaction.Commit();
                     }
-                }
-                catch (Exception e)
-                {
-                    var joke = e.Message.ToString();
-                    dbTransaction.Rollback();
-                    return false;
+                    else
+                    {
+                        model.InsDateTime = DateTime.Now;
+                        context.JobRequest.Add(model);
+                        context.SaveChanges();
+
+                        for (int i = 0; i < model.Car.Count; i++)
+                        {
+                            model.Car[i].JobRequestNoID = model.JobRequestNoID;
+                            model.Car[i].InsDateTime = DateTime.Now;
+                            context.Car.Add(model.Car[i]);
+                            context.SaveChanges();
+                            for (int k = i; k < model.Box.Count;)
+                            {
+                                model.Box[k].CarID = model.Car[i].CarID;
+                                model.Box[k].InsDateTime = DateTime.Now;
+                                context.Box.Add(model.Box[k]);
+                                context.SaveChanges();
+                                break;
+                            }
+
+                        }
+
+                    }
+                    //context.SaveChanges();
+                    //dbTransaction.Commit();
                 }
             }
+            catch (Exception e)
+            {
+                var joke = e.Message.ToString();
+                //dbTransaction.Rollback();
+                return false;
+            }
+            //}
             return true;
         }
         public object GetJobWorkList(int jobstatusID)
